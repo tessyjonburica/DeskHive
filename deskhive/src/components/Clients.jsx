3f076dab4975c859e04a355c9648e0dc0f0598cf
Teslim <yusuftessy2005@gmail.com>
2024-06-11 16:32:34 +0100

    clients updated

--- deskhive/src/components/Clients.jsx
+++ deskhive/src/components/Clients.jsx
@@ -11,14 +11,19 @@ function Clients() {
           { name: 'Google', image: google },
           { name: 'Microsoft', image: microsoft },
           { name: 'Facebook', image: facebook },
-        ].map((client, index) =>
-          <div key={index} className="w-full rounded-xl bg-gray-100 p-4">
-            <img src={client.image} alt={client.name} className="w-full h-auto mx-auto" />
+        ].map((client, index) => (
+          <div key={index} className="w-full rounded-xl bg-gray-100 p-4 flex flex-col items-center">
+            <img
+              src={client.image}
+              alt={client.name}
+              className="w-24 h-24 sm:w-32 sm:h-32 object-contain"
+            />
             <div className="text-center mt-4 text-gray-500">{client.name}</div>
           </div>
-        )}
+        ))}
       </div>
     </div>
+
   )
 }
 
