1ebb33ca1b062e7af2a4ca95fe3af7477855311e
Teslim <yusuftessy2005@gmail.com>
2024-07-05 06:51:06 +0100

    Added @vercel/analytics dependency and imported Analytics component in App.jsx

--- deskhive/src/App.jsx
+++ deskhive/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import "./App.css";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
@@ -13,20 +14,24 @@ import Confirmation from "./pages/Confirmation";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
+     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+          {/* <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="services" element={<Services />} />
-          <Route path="desk-selection" element={<DeskSelection />} />
+          <Route path="desk-selection" element={<DeskSelection />} /> */}
           <Route path="checkout" element={<Checkout/>} />
           <Route path="confirmation" element={<Confirmation />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
+    <Analytics />
+    </>
+   
   );
 }
 
@@ -34,36 +39,3 @@ export default App;
 
 
 
-
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import "./App.css";
-// import Contact from "./pages/Contact";
-// import Home from "./pages/Home";
-// import Layout from "./pages/Layout";
-// import About from "./pages/About";
-// import Services from "./pages/Services";
-// import NoPage from "./pages/NoPage";
-// import DeskSelection from "./components/DeskSelection";
-// import Booking from "./pages/Booking";
-
-// function App() {
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Layout />}>
-//             <Route index element={<Home />} />
-//             <Route path="about" element={<About />} />
-//             <Route path="contact" element={<Contact />} />
-//             <Route path="services" element={<Services />} />
-//             <Route path="desk-selection" element={<DeskSelection />} />
-//             <Route path="booking" element={<Booking />} />
-//             <Route path="*" element={<NoPage />} />
-//           </Route>
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-
-// export default App;
