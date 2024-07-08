a67712819c4ecc81ad78c4471a898feb5a207042
Teslim <yusuftessy2005@gmail.com>
2024-07-08 19:03:04 +0100

    Added @vercel/speed-insights dependency and imported SpeedInsights component in App.jsx

--- deskhive/src/App.jsx
+++ deskhive/src/App.jsx
@@ -15,23 +15,24 @@ import Confirmation from "./pages/Confirmation";
 function App() {
   return (
     <>
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          {/* <Route path="about" element={<About />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            {/* <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="services" element={<Services />} />
           <Route path="desk-selection" element={<DeskSelection />} /> */}
-          <Route path="checkout" element={<Checkout/>} />
-          <Route path="confirmation" element={<Confirmation />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    <Analytics />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="confirmation" element={<Confirmation />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+      <SpeedInsights />
     </>
-   
+
   );
 }
 
